260e1724d8071f83cd51703dd028e078cbaf09f6
Eng-Emad <mail.emad.hamdi@gmail.com>
2025-05-27 13:32:15 +0300

    Refactor CSS styles for improved organization; enhance login and fetch functionality in JavaScript

--- js/login.js
+++ js/login.js
@@ -2,58 +2,88 @@
 const loginSection = document.getElementById('login');
 const registerForm = document.getElementById('register-form');
 const loginForm = document.getElementById('login-form');
-	const adminUser ={
-		email: "emad@admin.com",
-		password: "123"
-	}
-	localStorage.setItem("adminUser", JSON.stringify(adminUser));
-	let userAdmin = JSON.parse(localStorage.getItem("adminUser"));
+// Add admin Data 
+const adminUser = {
+	email: "emad@admin.com",
+	password: "123"
+}
+// Set admin user in localStorage
+localStorage.setItem("adminUser", JSON.stringify(adminUser));
+
+let userAdmin = JSON.parse(localStorage.getItem("adminUser"));
 // pull Data in Elements 
 registerForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
+
 	let name = document.getElementById('regName').value;
 	let email = document.getElementById('regEmail').value;
 	let password = document.getElementById('regPassword').value;
+
 	// Values ​​test  
-	if(!email || !password || !name) {
+	if (!email || !password || !name) {
+
 		alert('Please fill in all fields 🥺');
+
 		return;
 	}
-	if(!validateEmail(email)) {
+
+	if (!validateEmail(email)) {
+
 		alert('Please enter a valid email address 🥺');
+
 		return;
 	}
+
 	// set data in localStorage
+
 	localStorage.setItem("user", JSON.stringify({
 		name,
 		email,
 		password
 	}))
-	alert('Registeration successful! 🎉');
-// reset the form
-clearForm(registerForm);
-})
 
+	// alert('Registeration successful! 🎉');
+
+	// Reset the form
+	clearForm(registerForm);
+})
+// Login logic 
 loginForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
+
 	let email = document.getElementById('loginEmail').value;
+
 	let password = document.getElementById('loginPassword').value;
+
+	// Get user in localStorage 
+
 	let userData = JSON.parse(localStorage.getItem("user"));
-	if(!loginEmail || !loginPassword) {
+
+	if (!loginEmail || !loginPassword) {
+
 		alert('Please fill in all fields 🥺');
+
 		return;
 	}
+	// Check login 
+	if (userData && email === userData.email && password === userData.password) {
 
-	if(userData && email === userData.email && password === userData.password) {
 		alert('Welcome back ' + userData.name + '! 🎉');
+
 		localStorage.setItem("isLoggedIn", true);
+
 		window.location.href = 'home.html';
-	}else if( email===adminUser.email && password === adminUser.password) {
-		alert('Welcome Admin! 🎉');
-	window.location.href = 'admin.html';
-	}else{
+
+	} else if (email === adminUser.email && password === adminUser.password) {
+
+		// alert('Welcome Admin! 🎉');
+		// Page redirection to Admin page 
+		window.location.href = 'admin.html';
+
+	} else {
+
 		alert('Invalid email or password 🥺');
-		
+
 	}
 })
 
@@ -70,12 +100,7 @@ function validateEmail(email) {
 }
 // clear form
 function clearForm(form) {
+
 	form.reset();
+
 }
-
-
-
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -11,23 +11,31 @@ navbarBtn.addEventListener('click', function () {
 // Logout Button 
 document.getElementById('login').addEventListener('click', function () {
 	localStorage.removeItem("user");
+	// Page redirection 
 	window.location.href = "./index.html";
 })
 
 
 // fetch data 
 let allProducts = [];
+
 async function fetchData() {
 		try {
+
 		let resp =await fetch ('https://fakestoreapi.com/products');
 		let data = await resp.json(); 
+
 		// repeat products to get 100 product
 		allProducts= Array(100).fill(data).flat();
 
 		const categories = [...new Set(allProducts.map(product => product.category))];
 		filterProducts(categories);
 		displayProducts(allProducts);
-	} catch (error) {
+	} 
+
+	// catch Error 
+
+	catch (error) {
 		console.error('Error fetching data:', error);
 		productsContainer.innerHTML = '<p>Error loading products. Please try again later.</p>';
 	}
@@ -36,7 +44,9 @@ async function fetchData() {
 
 // Filter Buttons 
 function filterProducts(category) {
+
 	const allBtn = document.createElement('button');
+
 	allBtn.addEventListener('click', () => {
 		displayProducts(allProducts);
 	});
@@ -45,21 +55,33 @@ function filterProducts(category) {
 	// Create Button filter
 	category.forEach(element => {
 		const btn = document.createElement('button');
+
 		btn.className = 'btn btn-dark mx-1 my-1';
+
 		btn.textContent = element;
+
 		btn.addEventListener('click', () => {
+
 			const filteredProducts = allProducts.filter(product => product.category === element);
+
 			displayProducts(filteredProducts);
 		});
+
 		filter.appendChild(btn);
+
 	});
 }
-// displayProducts 
+// display products 
 function displayProducts(products) {
+
 	productsContainer.innerHTML = '';
+
 	products.forEach(product => {
+
 		const productCard = document.createElement('div');
+
 		productCard.className = 'col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center';
+		// display cards 
 		productCard.innerHTML = `
 		<div class="card" style="width: 18rem; height:27rem;">
 			<img src="${product.image}" alt="${product.title} " class="card-img-top h-50" >
@@ -69,7 +91,9 @@ function displayProducts(products) {
 		 <button type="button" class="btn btn-outline-dark btn-add-card">Add to Card</button>
 		  </div>
  </div>`;
+
 		productsContainer.appendChild(productCard);
+
 	});
 }
 fetchData();
